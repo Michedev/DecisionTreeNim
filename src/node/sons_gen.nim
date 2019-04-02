77ebb898bb5f2e658bc6dfe6657855ea44ba8795
Mikedev <mik3dev@gmail.com>
2019-04-02 18:05:38 +0200

    removed recursive creation of sons

--- src/node/sons_gen.nim
+++ src/node/sons_gen.nim
@@ -1,23 +1,27 @@
 import node, split, constructors, stop, leaf
+import typetraits
 
-proc generate_sons(n: Node, X: seq[seq[float]], y: seq[float]): Node =
+type Sons = tuple[first, second: Node, X1, X2: seq[seq[float]], y1, y2: seq[float]]
+
+proc generate_sons*(n: Node, X: seq[seq[float]], y: seq[float]): Sons =
     let split: SplitResult = best_split[true](n, X, y)
+    let
+        x1_len = split.index[0].len
+        x2_len = split.index[1].len
     var 
-        X1 = new_seq[seq[float]]()
-        y1 = new_seq[float]()
-        X2 = new_seq[seq[float]]()
-        y2 = new_seq[float]()
+        X1 = new_seq[seq[float]](x1_len)
+        y1 = new_seq[float](x1_len)
+        X2 = new_seq[seq[float]](x2_len)
+        y2 = new_seq[float](x2_len)
         X_splitted = [X1, X2]
         y_splitted = [y1, y2]
     for i in [0,1]:
-        for indx in split.index[i]:
-            X_splitted[i].add(X[indx])
-            y_splitted[i].add(y[indx])
+        for i_indx, indx in split.index[i]:
+            X_splitted[i][i_indx] = X[indx]
+            y_splitted[i][i_indx] = y[indx]
     for i in [0, 1]:
         if stop.on_creating_new_node(n, X_splitted[i], y_splitted[i]):
             n.sons[i] = new_leaf(n, X_splitted[i], y_splitted[i])
         else:
             n.sons[i] = new_node(n)
-    for i, son in n.sons:
-        if not(son is Leaf):
-            let _ = son.generate_sons(X_splitted[i], y_splitted[i])
+    return (n.sons[0], n.sons[1], X_splitted[0], X_splitted[1], y_splitted[0], y_splitted[1])
