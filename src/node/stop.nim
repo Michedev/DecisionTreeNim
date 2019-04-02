2d2a02b1c091108418322f71a1b58f73574b26b3
Mikedev <mik3dev@gmail.com>
2019-04-02 19:23:58 +0200

    add more control to stop; in classification when one class if present when there are 1 or less instances

--- src/node/stop.nim
+++ src/node/stop.nim
@@ -1,4 +1,15 @@
 import node
+import ../task
+
+proc unique_class(y: seq[float]): bool =
+    for value in y:
+        if value != y[0]:
+            return false
+    echo y, " has one class"
+    return true
 
 proc on_creating_new_node*(n: Node, X: seq[seq[float]], y: seq[float]): bool =
-    n.level >= 3
+    if n.task == Classification:
+        if unique_class(y):
+            return true
+    return n.level >= 3 or y.len <= 1
