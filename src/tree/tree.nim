c457d5aab6dc592465172725901e3c1c7b1d9d0c
Mikedev <mik3dev@gmail.com>
2019-04-02 18:05:57 +0200

    start to implement tree

--- src/tree/tree.nim
+++ src/tree/tree.nim
@@ -1,13 +1,23 @@
-import node/[node, constructors, split]
+import ../node/[node, constructors, split, sons_gen]
+import ../task
 
 type DecisionTree* = ref object
     root: Node
 
+proc new_tree(task: Task): DecisionTree =
+    result = new(DecisionTree)
+    result.root = new_root(task)
 
 
-proc fit(t: DecisionTree, X: seq[seq[float]], y: seq[float]): DecisionTree =
-    t.root = new_node()
-    let split_info = t.root.best_split(X,y)
-    t.root.split_column = split_info.col
+proc new_classification_tree(): DecisionTree = new_tree(task=Classification) 
 
-    return t
+
+proc new_regression_tree(): DecisionTree = new_tree(task=Regression)
+
+
+proc fit(t: DecisionTree, X: var seq[seq[float]], y: var seq[float]): DecisionTree =
+    var border: seq[Node] = new_seq[Node](0)
+    border[0] = t.root
+    while border.len > 0:
+        let node = border.pop()
+        let sons = node.generate_sons(X, y)
