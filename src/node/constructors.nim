08f2e5be4e73c09cc5f7a66761cdd776ce9f4f9a
Mikedev <mik3dev@gmail.com>
2019-04-02 19:22:54 +0200

    add root optional argument impurity function

--- src/node/constructors.nim
+++ src/node/constructors.nim
@@ -1,20 +1,30 @@
 import node
 import leaf
 import ../task
+import ../impurity
 
 
 proc new_leaf*(father: Node, X: seq[seq[float]], y: seq[float]): Leaf =
     result = new(Leaf)
     result.level = father.level + 1
     result.task = father.task
+    result.impurity = father.impurity
     result.leaf_f = result.get_leaf_func(X, y)
 
 proc new_node*(father: Node): Node =
     result = new(Node)
     result.level = father.level + 1
+    result.impurity = father.impurity
     result.task = father.task
 
-proc new_root*(task: Task): Node =
+proc new_root*(task: Task, impurity: proc(y: seq[float]): float = nil): Node =
     result = new(Node)
     result.level = 0
     result.task = task
+    if impurity.is_nil():
+        if task == Classification:
+            result.impurity = gini
+        else:
+            result.impurity = mse_from_mean
+    else:
+        result.impurity = impurity
