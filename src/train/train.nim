81fe72e1032625b4018cc095c4e4b6e6fbe12aa8
Mikedev <mik3dev@gmail.com>
2019-09-15 01:14:59 +0200

    moved fit into train/train.nim

---
+++ src/train/train.nim
@@ -0,0 +1,32 @@
+import ../node/[node, constructors]
+import sons_gen
+import options
+
+type NodeWithData = tuple[n: Node, X: seq[seq[float]], y: seq[float]]
+
+## Train function of decision tree
+proc fit* (root: Node, X: seq[seq[float]], y: seq[float]) =
+    assert X.len == y.len
+    var border = new_seq[NodeWithData](1)
+    border[0] = (root, X, y)
+    while border.len > 0:
+        let (node, X_data, y_data) = border.pop()
+        let sons_opt = node.generate_sons(X_data, y_data)
+        if isSome(sons_opt):
+            let sons = sons_opt.get()
+            if not(sons.first of Leaf):
+                border.add((sons.first, sons.X1, sons.y1))
+            if not(sons.second of Leaf):
+                border.add((sons.second, sons.X2, sons.y2))
+        elif not node.father.isNil():
+            let father = node.father
+            if father.num_sons == 0:
+                father.sons[0] = new_leaf(father, X_data, y_data)
+            else:
+                for i in 0..<father.num_sons:
+                    let son = father.sons[i]
+                    if son == node:
+                        father.num_sons -= 1
+                        father.sons[i] = new_leaf(father, X_data, y_data)
+        else:
+            raise newException(RootIsLeaf, "Root is leaf")
--- src/tree.nim
+++ src/tree.nim
@@ -1,9 +1,11 @@
-import node/[node, constructors, split, sons_gen, traverse]
+import node/[node, constructors, traverse]
+import train/[split, sons_gen, train]
 import task
 import typetraits
 import core.typeinfo
 import math
 import sequtils
+import options
 import rule/[tree_rules, stop_rules]
 
 type 
@@ -13,7 +15,6 @@ type
         max_features: float
         max_depth: int
         min_samples_split: int
-    NodeWithData = tuple[n: Node, X: seq[seq[float]], y: seq[float]]
 
 proc new_tree (task: Task, max_depth, min_samples_split: int, max_features: float): DecisionTree =
     result = new(DecisionTree)
@@ -24,33 +25,37 @@ proc new_tree (task: Task, max_depth, min_samples_split: int, max_features: floa
     if min_samples_split != -1:
         result.grow_rules.stop_rules.add_pre_split_rule min_samples_split_rule(min_samples_split)
     result.grow_rules.stop_rules.add_creation_rule unique_class_rule()
-    result.grow_rules.stop_rules.add_creation_rule impurity_rule(0.01)
+    result.grow_rules.stop_rules.add_post_split_rule min_impurity_decrease(0.0001)
     result.max_features = max_features
 
 
-proc new_classification_tree* (max_depth: int = -1, min_samples_split: int = 1, max_features: float = 1.0): DecisionTree = 
+proc new_classification_tree* (max_depth: int = -1, min_samples_split: int = -1, max_features: float = 1.0): DecisionTree = 
     new_tree(task=Classification, max_depth, min_samples_split, max_features) 
 
 proc new_regression_tree* (max_depth: int = -1, min_samples_split: int = 1, max_features: float = 1.0): DecisionTree = 
     new_tree(task=Regression, max_depth, min_samples_split, max_features)
 
+## Train function of decision tree
 proc fit* (t: DecisionTree, X: seq[seq[float]], y: seq[float]) =
-    assert X.len == y.len
-    var border= new_seq[NodeWithData](1)
-    border[0] = (t.root, X, y)
-    while border.len > 0:
-        let (node, X_data, y_data) = border.pop()
-        let sons = node.generate_sons(X_data, y_data)
-        if not(sons.first of Leaf):
-            border.add((sons.first, sons.X1, sons.y1))
-        if not(sons.second of Leaf):
-            border.add((sons.second, sons.X2, sons.y2))
+    try:
+        fit(t.root, X, y)
+    except RootIsLeaf:
+        t.root = new_root_leaf(X,y)
 
 proc print_root_split*(t: DecisionTree) =
-    echo "Split in column ", t.root.split_column, " with value ", t.root.split_value
+    if t.root of Leaf:
+        echo "Root is a leaf"
+    else:
+        echo "Root node, split in column ", t.root.split_column, " with value ", t.root.split_value
             
 proc predict*(tree: DecisionTree, x: seq[float]): float =
     tree.root.get_value(x)
 
 proc predict*(tree: DecisionTree, X: seq[seq[float]]): seq[float] =
     X.map_it(tree.predict(it))
+
+proc predict_proba*(tree: DecisionTree, x: seq[float]): seq[float] =
+    tree.root.get_proba(x)
+
+proc predict_proba*(tree: DecisionTree, X: seq[seq[float]]): seq[seq[float]] =
+    X.map_it(tree.predict_proba(it))
