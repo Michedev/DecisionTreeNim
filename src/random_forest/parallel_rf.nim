d64f1433613d216d95fbc52c2643b8d3d47e92bf
Mikedev <mik3dev@gmail.com>
2019-09-26 18:20:35 +0200

    some fixes

--- src/random_forest/parallel_rf.nim
+++ src/random_forest/parallel_rf.nim
@@ -34,29 +34,27 @@
 #             of Regression:
 #                 return results.mean()
 
-proc fit(forest: seq[DecisionTree], X: seq[seq[float]], y: seq[float]): bool {.thread.} =
+
+#for parallel use only
+proc fit(forest: seq[DecisionTree], X: seq[seq[float]], y: seq[float]): void=
     for tree in forest:
         tree.fit(X,y)
-    return true
 
 proc fit_parallel*(forest: RandomForest, X: seq[seq[float]], y: seq[float]) =
     let trees_per_thread: int = (forest.num_trees.float / forest.num_threads.float).int
     var thread_results = new_seq[bool](forest.num_threads)
-    for i_thread in 1||forest.num_threads:
-        let trees_slice = if(i_thread < forest.num_threads):
+    for i_thread in 0||(forest.num_threads-1):
+        let trees_slice = if(i_thread < forest.num_threads-1):
             (i_thread * trees_per_thread)..<((i_thread+1) * trees_per_thread)
         else: (i_thread*trees_per_thread)..<forest.trees.len
-        thread_results[i_thread - 1] = fit(forest.trees[trees_slice], X, y)
+        fit(forest.trees[trees_slice], X, y)
     
-    
-
-
 proc predict_proba_parallel*(forest: RandomForest, x: seq[float], single_predict: proc(f: RandomForest, x: seq[float]): seq[float] {.gcsafe.}): seq[float] =
     let trees_per_thread: int = (forest.num_trees.float / forest.num_threads.float).int
     result = new_seq[float](forest.num_classes)
     var thread_results = new_seq[seq[float]](forest.num_threads)
-    for i_thread in 1||forest.num_threads:
-        let trees_slice = if(i_thread < forest.num_threads):
+    for i_thread in 0..(forest.num_threads-1):
+        let trees_slice = if(i_thread < (forest.num_threads-1)):
                 (i_thread * trees_per_thread)..<((i_thread+1) * trees_per_thread)
             else: (i_thread*trees_per_thread)..<forest.trees.len
         let subtrees = forest.trees[trees_slice]
@@ -64,7 +62,7 @@ proc predict_proba_parallel*(forest: RandomForest, x: seq[float], single_predict
         subforest.num_classes = forest.num_classes
         subforest.num_threads = 1
         subforest.task = forest.task
-        thread_results[i_thread - 1] = single_predict(subforest, x)
+        thread_results[i_thread] = single_predict(subforest, x)
     for thread_result in thread_results:
         let p_subtrees = thread_result
         for i in 0..<forest.num_classes:
@@ -78,8 +76,8 @@ proc predict_parallel*(forest: RandomForest, x: seq[float], single_predict: proc
     let trees_per_thread: int = (forest.num_trees.float / forest.num_threads.float).int
     var results = new_seq[float](0)
     var thread_results = new_seq[float](forest.num_threads)
-    for i_thread in 1||forest.num_threads:
-        let trees_slice = if(i_thread < forest.num_threads):
+    for i_thread in 0||(forest.num_threads-1):
+        let trees_slice = if(i_thread < (forest.num_threads-1)):
                 (i_thread * trees_per_thread)..<((i_thread+1) * trees_per_thread)
             else: (i_thread*trees_per_thread)..<forest.trees.len
         let subtrees = forest.trees[trees_slice]
@@ -87,11 +85,10 @@ proc predict_parallel*(forest: RandomForest, x: seq[float], single_predict: proc
         subforest.num_classes = forest.num_classes
         subforest.num_threads = 1
         subforest.task = forest.task
-        thread_results[i_thread - 1] = single_predict(subforest, x)
-        results.add thread_results[i_thread - 1]
+        thread_results[i_thread] = single_predict(subforest, x)
+        results.add thread_results[i_thread]
     case forest.task:
         of Classification:
             return results.toCountTable().largest.key
         of Regression:
             return results.mean()
-                
