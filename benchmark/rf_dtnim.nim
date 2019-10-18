e7efca2b114ccb7e5bc75097ee54b212a91dfa27
Mikedev <mik3dev@gmail.com>
2019-10-18 16:24:38 +0200

    update benchmarks

--- benchmark/rf_dtnim.nim
+++ benchmark/rf_dtnim.nim
@@ -2,14 +2,24 @@ import ../src/DecisionTree
 import ../tests/test_utils
 import times
 # import nimprof
+import math
 
 proc main(): void =
     let X_iris = read_X_data("tests/data/X_iris.csv")
     let y_iris = read_y_data("tests/data/y_iris.csv")
-    let rf = new_random_forest_classifier(100, num_threads=1)
-    var start = now()
-    rf.fit(X_iris, y_iris)
-    let time_fit = now() - start
-    echo "seconds fit: ", time_fit.inMilliseconds.float / 1000
+    var rf = new_random_forest_classifier(10, num_threads=1)
+    # var start = now()
+    # rf.fit(X_iris, y_iris)
+    # let time_fit = now() - start
+    # echo "seconds fit: ", time_fit.inMilliseconds.float / 1000
+    rf = new_random_forest_classifier(1, num_threads=1, max_depth=10, min_samples_split=10)
+    let X_train_adult = read_X_data("tests/data/X_train_adult.csv")
+    let y_train_adult = read_y_data("tests/data/y_train_adult.csv")
+    let X_test_adult = read_X_data("tests/data/X_test_adult.csv")
+    let y_test_adult = read_y_data("tests/data/y_test_adult.csv")
+    rf.fit(X_train_adult, y_train_adult)
+    let yhat_test = rf.predict(X_test_adult)
+    echo "Accuracy test: ", accuracy(y_test_adult, yhat_test)
+    echo "Perc 1 prediction: ", yhat_test.sum().float / yhat_test.len.float
 
 main()
--- benchmark/rf_scikit.py
+++ benchmark/rf_scikit.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 from path import Path
+from sklearn.metrics import accuracy_score
 
 def main():
     rf = RandomForestClassifier(n_estimators=100)
@@ -17,6 +18,11 @@ def main():
         end = time.time()
         times[i] = end - start
     print(f'Mean time (s) : {np.mean(times)} std time(s): {np.std(times)}')
+    X, y = pd.read_csv(data_f / 'X_train_adult.csv', header=None), pd.read_csv(data_f / 'y_train_adult.csv', header=None)
+    X_test, y_test = pd.read_csv(data_f / 'X_test_adult.csv', header=None), pd.read_csv(data_f / 'y_test_adult.csv', header=None)
+    rf.fit(X,y)
+    yhat = rf.predict(X_test)
+    print(f"Accuracy test adult: {accuracy_score(y_test, yhat)}")
 
 if __name__ == '__main__':
     main()
