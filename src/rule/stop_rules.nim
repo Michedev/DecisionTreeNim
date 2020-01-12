e693ab13a4337938c0408b295e3001f672d6b321
Mikedev <mik3dev@gmail.com>
2020-01-12 22:38:30 +0100

    fixed error min impurity_decrease add name functions

--- src/rule/stop_rules.nim
+++ src/rule/stop_rules.nim
@@ -10,19 +10,22 @@ proc max_depth_rule*(max_depth: int): Rule =
     (proc(n: INode, X: MatrixView[float32], y: VectorView[float32]): bool {.gcsafe.} = n.level > max_depth)
 
 proc unique_class_rule*(): Rule =
-    (proc(n: INode, X: MatrixView[float32], y: VectorView[float32]): bool {.gcsafe.} =
+    proc is_unique_class(n: INode, X: MatrixView[float32], y: VectorView[float32]): bool {.gcsafe.} =
+        if y.len == 0:
+            return true
         let first_el = y[0]
         for el in y:
             if el != first_el:
                 return false
-        return true)
+        return true
+    return is_unique_class
 
 proc min_impurity_decrease_rule*(threshold: float32): PostSplitRule =
-    (
-        proc(n: INode, X: MatrixView[float32], y: VectorView[float32], X1: MatrixView[float32], y1: VectorView[float32], X2: MatrixView[float32], y2: VectorView[float32]): bool {.gcsafe.} =
-            # let decrease = n.impurity_f(y.to_seq) - (n.impurity_f(y1.to_seq) + n.impurity_f(y2.to_seq)) 
-            # # echo"decrease is ", decrease 
-            # return decrease < threshold
-            return false
-        
-    )
+    proc is_impurity_less(n: INode, X: MatrixView[float32], y: VectorView[float32], X1: MatrixView[float32], y1: VectorView[float32], X2: MatrixView[float32], y2: VectorView[float32], split: SplitResult): bool {.gcsafe.} =
+        ## Stop if the decrease of split impurity is less than threshold
+        let perc_1 = X1.len.float32 / X.len.float32
+        let perc_2 = X2.len.float32 / X.len.float32
+
+        let decrease = n.impurity_value - (perc_1 * split.impurity_1 + perc_2 * split.impurity_2)
+        return decrease < threshold        
+    return is_impurity_less
