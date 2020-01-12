dec8bcfcc78024eaedb1961edcc9fbc54cb65e3c
Mikedev <mik3dev@gmail.com>
2020-01-12 22:43:02 +0100

    fix sample add mean function for float32

--- src/utils.nim
+++ src/utils.nim
@@ -11,10 +11,16 @@ proc sample_wo_reins*(start, finish: int, perc: float32): seq[int] =
         let len_sub = (series.len.float32 * perc).round.int
         result = new_seq[int](len_sub)
         for i in 0..<len_sub:
-            let i_sample = rand(series.len)
+            let i_sample = rand(series.len-1)
             let element = series[i_sample]
             series.del i_sample
             result[i] = element
+
+proc mean*(s: seq[float32]): float32 =
+    result = 0.0
+    for v in s:
+        result += v
+    result /= s.len.float32
 
 template `:=`*[T](a,b: seq[T]): untyped =
     shallowCopy(a,b)
